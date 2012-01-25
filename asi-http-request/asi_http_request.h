5b63298ac5869ef353bb8a101d2b2b904f7464f8
Ed Gonzalez <edward.gonzalez@jtv.com>
2012-01-25 14:22:13 -0500

    Initial Commit

---
+++ asi-http-request/asi_http_request.h
@@ -0,0 +1,13 @@
+//
+//  asi_http_request.h
+//  asi-http-request
+//
+//  Created by Gonzalez, Edward on 1/25/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface asi_http_request : NSObject
+
+@end
---
+++ asi-http-request/asi_http_request.m
@@ -0,0 +1,23 @@
+//
+//  asi_http_request.m
+//  asi-http-request
+//
+//  Created by Gonzalez, Edward on 1/25/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "asi_http_request.h"
+
+@implementation asi_http_request
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
